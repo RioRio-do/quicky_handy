7613f378923136e8f0c7739131fa704050016d76
RioRio-do <riorio081121@gmail.com>
2024-05-08 21:36:59 +0900

    modified:   views/template.py

--- views/template.py
+++ views/template.py
@@ -1,23 +1,30 @@
 import flet as ft
 
-def microwave(page):
-    def calcS(e):
-        textS2.value = round(int(textW1.value) / int(textW2.value) * int(textS1.value))
-        ft.Page.update(page)
-    
-    def calcW(e):
-        textW2.value = round(int(textW1.value) / int(textS2.value) * int(textS1.value))
-        ft.Page.update(page)
+def ctax(page):
+    def calc(e):
+        if extax.value == "" and intax.value != "" and rate.value != "":
+            extax.value = round(intax.value / (1 + (rate.value) * 0.01) ,3)
+            ft.Page.update(page)
+
+        if extax.value != "" and intax.value == "" and rate.value != "":
+            intax.value = round(extax.value * (1 + (rate.value) * 0.01) ,3)
+            ft.Page.update(page)
+
+        if extax.value != "" and intax.value != "" and rate.value == "":
+            rate.value = round(((intax.value / extax.value) * 100) - 100 ,3)
+            ft.Page.update(page)
+            
         
-    textW1 = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="ワット数[W]",hint_text="ここにレンジのワット数を入力...",icon=ft.icons.ELECTRIC_BOLT,)
-    textS1 = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="加熱時間[秒]",hint_text="ここにレンジの加熱時間を入力...",icon=ft.icons.TIMER_SHARP)
-    textW2 = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="ワット数[W]",hint_text="ここにレンジのワット数を入力...",icon=ft.icons.ELECTRIC_BOLT,on_change=calcS)
-    textS2 = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="加熱時間[秒]",hint_text="ここにレンジの加熱時間を入力...",icon=ft.icons.TIMER_SHARP,on_change=calcW)
+        
+    extax = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="税抜価格",hint_text="ここに税抜価格を入力...",icon=ft.icons.TIMER_SHARP,on_change=calc)
+    intax = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="税込価格",hint_text="ここに税込価格を入力...",icon=ft.icons.TIMER_SHARP,on_change=calc)
+    rate = ft.TextField(keyboard_type=ft.KeyboardType.NUMBER,label="税率[%]",hint_text="ここに税率を入力...",icon=ft.icons.TIMER_SHARP,on_change=calc)
     
-    return ft.View("/microwave",[
+    
+    return ft.View("/ctax",[
         ft.AppBar(
             
-            title=ft.Text("電子レンジ出力変換器"),
+            title=ft.Text("税込税抜変換器"),
             center_title=True,
             bgcolor="#e2f38e",
             leading=ft.IconButton(icon=ft.icons.HOME,on_click=lambda _: ft.Page.go(page,"/home"),),
@@ -29,27 +36,9 @@ def microwave(page):
                     ft.Column(
                         [
                             ft.Container(ft.Text("変換前",size=20,text_align=ft.TextAlign.CENTER),),
-                            textW1,
-                            textS1,
-                        ],
-                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    ),
-                    border_radius=10,
-                    padding=20,
-                    border=ft.border.all(1,"#43474e"),
-                    width=360,
-                    height=256,
-                ),
-                
-                ft.Container(width=100,disabled=True,content=ft.Icon(ft.icons.ARROW_RIGHT_ALT,size=100,color="#43474e")),
-                
-                ft.Container(
-                    ft.Column(
-                        [
-                            ft.Container(ft.Text("変換後",size=20,text_align=ft.TextAlign.CENTER),),
-                            textW2,
-                            textS2,
+                            extax,
+                            intax,
+                            rate,
                         ],
                         alignment=ft.MainAxisAlignment.SPACE_AROUND,
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
